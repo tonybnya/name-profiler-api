ecea83e0b9a420151ce112f35c6b09f3acdea57b
tonybnya <tonybnya@gmail.com>
2026-04-16 13:33:06 +0100

    feat: add function to classify age

--- utils.py
+++ utils.py
@@ -9,3 +9,13 @@ from uuid6 import uuid7
 
 def generate_uuid_v7() -> str:
     return str(uuid7())
+
+
+def classify_age(age: int) -> str:
+    if age <= 12:
+        return "child"
+    elif age <= 19:
+        return "teenager"
+    elif age <= 59:
+        return "adult"
+    return "senior"
