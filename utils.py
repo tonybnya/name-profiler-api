34581239690a59aec6141370db830fe431e327a8
tonybnya <tonybnya@gmail.com>
2026-04-16 13:39:05 +0100

    refactor: remove the harcoded nationalize api url

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ Description : Helper functions
 Author      : @tonybnya
 """
 
+import requests
 from uuid6 import uuid7
 
 
@@ -19,3 +20,15 @@ def classify_age(age: int) -> str:
     elif age <= 59:
         return "adult"
     return "senior"
+
+
+def fetch_nationality(name: str, nationalize_api_url: str) -> dict[str, int | str]:
+    r = requests.get(f"{nationalize_api_url}?name={name}")
+    data = r.json()
+
+    countries = data.get("country", [])
+    if not countries:
+        raise Exception("Nationalize returned an invalid response")
+
+    top = max(countries, key=lambda x: x["probability"])
+    return top
